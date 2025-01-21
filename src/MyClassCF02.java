a88fcb15e9097719ae28dedddf53c98db0895a84
ice123239 <6752410018@gmail.com>
2025-01-21 17:20:11 +0700

    v.6 areaProject01-02

---
+++ src/MyClassCF02.java
@@ -0,0 +1,18 @@
+
+public class MyClassCF02 {
+    public static void main(String[] args){
+        int score = 75;
+        if( score > 80) {
+            System.out.println("Grade A ");
+        }else if(score> 70 ){
+            System.out.println("Grade B ");
+        }else if(score> 60 ){
+            System.out.println("Grade C ");
+        }else if(score> 50 ){
+            System.out.println("Grade D ");
+        }else{
+            System.out.println("Grade F ");
+        }
+        System.out.println("----------------");
+    }
+}
